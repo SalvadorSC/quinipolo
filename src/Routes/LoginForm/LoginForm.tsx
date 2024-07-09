7e8af7947b5462f09417ba4e77045dee774350fb
salvador.sanchez@king.com <salvador.sanchez@king.com>
2024-07-09 09:15:40 +0200

    chore: Remove unused import and commented code in LoginForm component

--- src/Routes/LoginForm/LoginForm.tsx
+++ src/Routes/LoginForm/LoginForm.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./LoginForm.module.scss";
-import { SignIn, SignUp } from "@clerk/clerk-react";
-import MenuBar from "../../components/MenuBar/MenuBar";
+import { SignUp } from "@clerk/clerk-react";
+/* import MenuBar from "../../components/MenuBar/MenuBar"; */
 
 const LoginForm = () => {
   return (
     <div className={styles.loginContainer}>
-      <MenuBar />
+      {/* <MenuBar /> */}
       <SignUp />
     </div>
   );
