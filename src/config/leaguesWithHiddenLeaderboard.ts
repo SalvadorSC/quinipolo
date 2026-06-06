8a540b0465cfd665bdd6b2d27e57e4f754f64f6c
Salvador A. Sánchez Campos <58109205+SalvadorSC@users.noreply.github.com>
2026-06-06 12:58:11 +0200

    Update leaguesWithHiddenLeaderboard.ts

--- src/config/leaguesWithHiddenLeaderboard.ts
+++ src/config/leaguesWithHiddenLeaderboard.ts
@@ -1,6 +1,6 @@
 /** League UUIDs where leaderboard points/results must not be shown. */
 const LEAGUES_WITHOUT_LEADERBOARD_RESULTS = [
-  "4cae8d44-f3bd-42a5-a899-78e64fdb0181", // Sant Feliu
+  // "4cae8d44-f3bd-42a5-a899-78e64fdb0181", // Sant Feliu
 ];
 
 export function shouldHideLeaderboardResults(
